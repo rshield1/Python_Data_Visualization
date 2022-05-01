e46422ac9c9cd9103e9303e7f21d02d90e9df4cc
Robert Shields <you@example.com>
2022-04-30 18:42:01 -0700

    used matplotlib for line graph

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 
-squares = [1,4,9,16,25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
+plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 #set chart title and lable size
 ax.set_title("Square Numbers", fontsize=24)
@@ -10,6 +13,6 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 #set size of tick labels.
-ax.tick_params(axis="both", labelsize=14)
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
